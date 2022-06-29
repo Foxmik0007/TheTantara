05525993b37277ff5d7a579d6279000808c43003
Mika Rafaralahy <mrafa099@uottawa.ca>
2022-06-29 09:02:04 -0500

    css update

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import m2 from './images/2.png'
 import m3 from './images/3.png'
 import m4 from './images/4.png'
 import m5 from './images/5.png'
+import dani from './images/daniel-216-asset.jpg'
+import horoscope from './images/horoscope.png'
 
 const Home = () => {
 
@@ -17,21 +19,26 @@ const Home = () => {
                     <div class="col-6 col-md-4">
                         <img src={m2} alt="img" width={600}></img>
                     </div>
-                    <div class="col-6 col-md-4"><img src={m3} alt="img" width={600}></img></div>
+                    <div class="col-6 col-md-4">
+                        <img src={m3} alt="img" width={600}></img>
+                    </div>
                 </div>
 
                 <div class="row">
 
-
-                    <div class="col-12 col-md-8"><img src={m4} alt="img" width={600}></img></div>
-                    <div class="col-6 col-md-4"><img src={m5} alt="img" width={600}></img></div>
+                    <div class="col-12 col-md-8">
+                        <img src={m4} alt="img" width={600}></img>
+                    </div>
+                    <div class="col-6 col-md-4">
+                        <img src={m5} alt="img" width={600}></img>
+                    </div>
                 </div>
                 <div class="row">
                     <div class="col">
-                        <img src=".." alt="img"></img>
+                        <img src={dani} alt="img" width={300}></img>
                     </div>
                     <div class="col">
-                        <img src=".." alt="img"></img>
+                        <img src={horoscope} alt="img" width={500}></img>
                     </div>
                 </div>
             </div>
--- src/navigation.js
+++ src/navigation.js
@@ -2,13 +2,29 @@ import { Link } from "react-router-dom";
 import './App.css';
 
 const Navigation = () => {
+
+
+    function MouseOver(event) {
+        event.target.style.backgroundColor = '#FFFFFF';
+    }
+    function MouseOut(event){
+        event.target.style.background="";
+      }
+    
+    const linkStyle = {
+        margin: "1rem",
+        textDecoration: "none",
+        color: 'black',
+        padding: "0.75%",
+      };
+
     return ( 
-        <nav className="navbar">
-            <Link to="/">Page titre</Link>
-            <Link to="/Articles">Articles</Link>
-            <Link to="/Ventes">Ventes</Link>
-            <Link to="/Affaires">Affaires</Link>
-            <Link to="/Acheter">Acheter un journal</Link>
+        <nav class="navbar">
+            <Link to="/" style={linkStyle} onMouseOver={MouseOver} onMouseOut={MouseOut}>Page titre</Link>
+            <Link to="/Articles" style={linkStyle} onMouseOver={MouseOver} onMouseOut={MouseOut}>Articles</Link>
+            <Link to="/Ventes" style={linkStyle} onMouseOver={MouseOver} onMouseOut={MouseOut}>Ventes</Link>
+            <Link to="/Affaires" style={linkStyle} onMouseOver={MouseOver} onMouseOut={MouseOut}>Affaires</Link>
+            <Link to="/Acheter" style={linkStyle} onMouseOver={MouseOver} onMouseOut={MouseOut}>Acheter un journal</Link>
         </nav>
      );
 }
