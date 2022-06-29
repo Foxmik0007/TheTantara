58de1bc04d9256711c31aa4281f688a395303b0d
Mika Rafaralahy <mrafa099@uottawa.ca>
2022-06-29 09:51:58 -0500

    police update

--- src/components/Acheter.js
+++ src/components/Acheter.js
@@ -1,7 +1,9 @@
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
+import './../App.css';
 
 const Achat = () => {
+
     return (
         <div class="acheter">
             <h3>Voulez vous acheter un modèle papier de ce journal ? Completez le formulaire et vous recevrez votre commande par la poste</h3>
@@ -9,47 +11,47 @@ const Achat = () => {
             <Form>
                 <Form.Group className="mb-3" controlId="firstName">
                     <div class="row">
-                        <div class="col-2"> <Form.Label>Nom : </Form.Label></div>
+                        <div class="col-2"> <Form.Label> <b>Nom :</b> </Form.Label></div>
                         <div class="col-6"><Form.Control type="text" /></div>
                     </div>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="lastName">
                     <div class="row">
-                        <div class="col-2"> <Form.Label>Prenom : </Form.Label></div>
+                        <div class="col-2"> <Form.Label><b>Prenom : </b></Form.Label></div>
                         <div class="col-6"><Form.Control type="text"/></div>
                     </div>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="address">
                     <div class="row">
-                        <div class="col-2"> <Form.Label>Adresse : </Form.Label></div>
+                        <div class="col-2"> <Form.Label><b>Adresse : </b></Form.Label></div>
                         <div class="col-6"><Form.Control type="text"/></div>
                     </div>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="numeroPoste">
                     <div class="row">
-                        <div class="col-2"> <Form.Label>Numero de poste : </Form.Label></div>
+                        <div class="col-2"> <Form.Label><b>Numero de poste : </b></Form.Label></div>
                         <div class="col-6"><Form.Control type="text"/></div>
                     </div>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="phoneNumber">
                     <div class="row">
-                        <div class="col-2"> <Form.Label>Numero de telephone : </Form.Label></div>
+                        <div class="col-2"> <Form.Label><b>Numero de telephone : </b></Form.Label></div>
                         <div class="col-6"><Form.Control type="text"/></div>
                     </div>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="dateDeparution">
                     <div class="row">
-                        <div class="col-2"> <Form.Label>Date de parution du journal : </Form.Label></div>
+                        <div class="col-2"> <Form.Label><b>Date de parution du journal : </b></Form.Label></div>
                         <div class="col-6"><Form.Control type="text"/></div>
                     </div>
                 </Form.Group>
 
-                <Button variant="primary" type="submit">
+                <Button variant="dark" type="submit" >
                     Submit
                 </Button>
             </Form>
