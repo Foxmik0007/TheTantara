2d090020f7566d5795da1df5539e1276435513df
Mika Rafaralahy <mrafa099@uottawa.ca>
2022-08-01 12:29:56 -0500

    front edit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
-import m1 from './images/1.png'
+import m1 from './images/events4.jpg'
 import m2 from './images/2.png'
-import m3 from './images/3.png'
-import m4 from './images/4.png'
+import m3 from './images/events3.PNG'
+import m4 from './images/events2.jpg'
 import m5 from './images/5.png'
 import dani from './images/daniel-216-asset.jpg'
 import horoscope from './images/horoscope.png'
@@ -14,31 +14,34 @@ const Home = () => {
             <div class="TitresDuJour">
                 <div class="row">
                     <div class="col-6 col-md-4">
-                        <img src={m1} alt="img" width={600}></img>
+                        <img src={m1} alt="image de thomas Shelby" width={600}></img>
+                        <p> Thomas Shelby : Le rôle d'une vie </p>
                     </div>
                     <div class="col-6 col-md-4">
-                        <img src={m2} alt="img" width={600}></img>
-                    </div>
-                    <div class="col-6 col-md-4">
-                        <img src={m3} alt="img" width={600}></img>
+                        <img src={m3} alt="Image de cowboy patrouillant dans la nuit" width={600}></img>
+                        <p> "Them's Fighting Words" : violence, masculinité et cow-boy texan à la fin du XIXe siècle </p>
                     </div>
                 </div>
 
                 <div class="row">
 
                     <div class="col-12 col-md-8">
-                        <img src={m4} alt="img" width={600}></img>
+                        <img src={m4} alt="Image d'une ville" width={600}></img>
+                        <p> 1948 dans le monde - L'après-guerre </p>
                     </div>
                     <div class="col-6 col-md-4">
-                        <img src={m5} alt="img" width={600}></img>
+                        <img src={m5} alt="Poster pour la promotion de la nouvelle voiture ford" width={600}></img>
+                        <p> Le nouvel invention de FORD </p>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col">
-                        <img src={dani} alt="img" width={300}></img>
+                        <img src={dani} alt="Image avis de recherche" width={300}></img>
+                        <p> Homme Recherché </p>
                     </div>
                     <div class="col">
-                        <img src={horoscope} alt="img" width={500}></img>
+                        <img src={horoscope} alt="image des horoscope" width={500}></img>
+                        <p> Carte des etoiles pour connaitre votre avenir </p>
                     </div>
                 </div>
             </div>
